7286a99ddd1a6f1b22a4d08da7e3272d81b796f9
vitoriafofa1021b <vitoria.santos9@estudante.ifms.edu.br>
2023-10-26 10:11:45 -0400

    COMMIT

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,39 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 //Tenho que ter uma rota post para cadastrar um filme
+
+//Salvar em algum lugar o filme que foi cadastrado.
+type Filme = {
+    id:number,
+    titulo:string,
+    descricao:string,
+    imagem:string
+}
+let filmesCadastros:Filme[] = []
 app.post('/filmes',(req,res)=>{
+    const {id,titulo,descricao,imagem} = req.body
     const filme = {
-        titulo:'Vingadores',
-        descricao:'Filme dos herois da marvel',
-        imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        id,
+        titulo,
+        descricao,
+        imagem
     }
+    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
+    filmesCadastros.push(filme)
     res.status(201).send(filme)
 })
 app.get('/filmes',(req,res)=>{
     res.send("Filmes Listados com sucesso")
 })
+
+app.get('/filmes/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    //FIND para buscar um filme pelo id e retornar para o usuário
+    const filme = filmesCadastros.find(filme=>filme.id === id)
+    if(!filme) return res.status(404).send('Filme não encontrado')
+    res.status(200).send(filme)
+})
+
 //Tenho que iniciar o servidor na porta 3000
 app.listen(3000,()=>{
     console.log('Servidor rodando na porta 3000')
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,5 @@
 import {test,expect,describe} from 'vitest'
-import axios from 'axios'
+// import axios from 'axios'
 describe('Primeiro teste da aplicação',()=>{
     test('Espero que 1 seja igual a 1',()=>{
     expect(1).toBe(1)
@@ -8,14 +8,73 @@ describe('Primeiro teste da aplicação',()=>{
 describe('Cadastro de filmes',()=>{
     test('Espero que o filme seja cadastrado',async ()=>{
         const filme = {
+            id:1,
             titulo:'Vingadores',
             descricao:'Filme dos herois da marvel',
             imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
         }
         //Passar isso para minha API
-        const resposta = await axios.post('http://localhost:3000/filmes',filme)
+        const resposta = await fetch(
+                    'http://localhost:3000/filmes',
+                    {
+                        method:'POST',
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                        body:JSON.stringify(filme)
+                    }
+                    )
         //Esperar que minha API tenha cadastrado o filme
         expect(resposta.status).toBe(201)
-        expect(resposta.data).toEqual(filme)
+        expect(await resposta.json()).toEqual(filme)
+    })
+    test('Espero que cadastre um segundo filme',async ()=>{
+        const filme = {
+            id:2,
+            titulo:'Vingadores',
+            descricao:'Filme dos herois da marvel',
+            imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        }
+        //Passar isso para minha API
+        const resposta = await fetch(
+                    'http://localhost:3000/filmes',
+                    {
+                        method:'POST',
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                        body:JSON.stringify(filme)
+                    }
+                    )
+        //Esperar que minha API tenha cadastrado o filme
+        expect(resposta.status).toBe(201)
+        expect(await resposta.json()).toEqual(filme)
+    })
+    test('Espero que o filme cadastrado seja listado',async ()=>{
+        const filme = {
+            id:2,
+            titulo:'Vingadores',
+            descricao:'Filme dos herois da marvel',
+            imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        }
+        //Passar isso para minha API
+        const resposta = await fetch(
+                    'http://localhost:3000/filmes',
+                    {
+                        method:'POST',
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                        body:JSON.stringify(filme)
+                    }
+        )
+        //Esperar que minha API tenha cadastrado o filme
+        expect(resposta.status).toBe(201)
+        expect(await resposta.json()).toEqual(filme)
+
+        //Listar os filmes cadastrados
+        const respostaGet = await fetch('http://localhost:3000/filmes/2')
+        expect(respostaGet.status).toBe(200)
+        expect(await respostaGet.json()).toEqual(filme)
     })
 })
